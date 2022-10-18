976303e3d1d7c89f2eb6b391458d5f5c6128f5ac
최민호 <chlals862@naver.com>
2022-10-18 21:01:40 +0900

    clone update

---
+++ Medicine/src/main/webapp/resources/js/userPage/medicineInfo_main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	$("#resetBtn").on("click", function() {
+		$("#productName").val("");
+		$("#companyName").val("");
+		$("#ingredientName").val("");
+	});
+	//검색버튼 클릭시 해당 검색어로 입력된 상태로 페이지이동
+	
+	//제품명 검색
+	$(".searchPN").on("click", function() {
+		var pnVal = $(".searchTxt").val();
+		console.log(pnVal);
+		$("#searchGbn").val("1");
+		var searchVal = $("#searchGbn").val();
+		console.log(searchVal);
+		$("#actionForm").attr("action", "medicineInfoList");
+		$("#actionForm").submit();
+	});
+	//업체명 검색
+	$(".searchCN").on("click", function() {
+		var pnVal = $(".searchTxt2").val();
+		console.log(pnVal);
+		$("#searchGbn").val("2");
+		var searchVal = $("#searchGbn").val();
+		console.log(searchVal);
+		$("#actionForm").attr("action", "medicineInfoList");
+		$("#actionForm").submit();
+	});
+	//성분명 검색
+	$(".searchIN").on("click", function() {
+		var pnVal = $(".searchTxt3").val();
+		console.log(pnVal);
+		$("#searchGbn").val("3");
+		var searchVal = $("#searchGbn").val();
+		console.log(searchVal);
+		$("#actionForm").attr("action", "medicineInfoList");
+		$("#actionForm").submit();
+	});
+});
